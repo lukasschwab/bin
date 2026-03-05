e2334cb93def063dc3910cab33ff9c667c1d16a8
Lukas Schwab <lukas.schwab@gmail.com>
2026-03-05 14:23:37 -0800

    nvim: promote telescope to primary selector

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -131,8 +131,15 @@ require("lazy").setup({
   {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-ui-select.nvim",
+    },
     config = function()
+        require("telescope").setup({
+            extensions = { ["ui-select"] = { require("telescope.themes").get_dropdown() } },
+        })
+        require("telescope").load_extension("ui-select")
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
         vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
